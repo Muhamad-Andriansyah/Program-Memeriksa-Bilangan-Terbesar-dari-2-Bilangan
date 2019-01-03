8e372201f3ba4b155ba0757b692c2c9260cbe92e
Muhamad-Andriansyah <45659337+Muhamad-Andriansyah@users.noreply.github.com>
2019-01-03 08:28:32 +0700

    Add files via upload

---
+++ Program Memeriksa Bilangan Terbesar dari 2 Bilangan.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a,b;
+
+cout<<"Menentukan Bilangan Terbesar Dari Dua Buah Bilangan"<<endl;
+cout<<"MUHAMAD ANDRIANSYAH"<<endl;
+cout<<"311810297"<<endl;
+cout<<"Masukan Bilangan ke 1"<<endl;
+cin>>a;
+cout<<"Masukan Bilangan ke 2"<<endl;
+cin>>b;
+
+if(a>b)
+    cout<<"Maka Bilangan Terbesar Adalah"<<a<<endl;
+    else
+        cout<<"Maka Bilangan Terbesar Adalah"<<b<<endl;
+    return 0;
+}
